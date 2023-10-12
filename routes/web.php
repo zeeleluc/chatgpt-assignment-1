3aa3da85d9b558bfaafb3c225c835700c8be9d0d
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2023-10-12 16:04:53 -0400

    Redirect to tasks index by default

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::resource('tasks', TaskController::class);
 });
